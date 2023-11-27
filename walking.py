dfb8245aa8f22cb641b0bab33aaa0df4e6031665
saitou-star <program0719.saitou@gmail.com>
2023-11-27 14:36:35 +0900

    特殊敵まで作成

--- walking.py
+++ walking.py
@@ -2,9 +2,10 @@ import pygame
 import sys
 import random
 
-# ゾンビをよけるゲーム、敵の数を減らす、リプレイできるようにする:リプレイ＝スペース⇒画面クリック
-# 敵のspeedをランダムでバラバラにしたい、敵の画像を2種類のゾンビ、敵画像の大きさを統一したい、
-# 敵のスピードで大きさが変わる、背景に画像を設定、playerに画像を設定、他仕掛けを作る
+# ゾンビをよけるゲーム、　敵の数を減らす、　リプレイできるようにする:リプレイ＝スペース⇒画面クリック
+# 敵のspeedをランダムでバラバラにしたい、　敵の画像を2種類のゾンビ、　敵画像の大きさを統一したい、
+# 敵のスピードで大きさが変わる、　背景に画像を設定、　playerに画像を設定、　7秒ごとに特殊敵出現
+# 特殊敵の大きさ調整、　他仕掛けを作る
 
 
 
@@ -28,8 +29,6 @@ player_width = 50
 player_height = 50
 player_x = (screen_width - player_width) // 2
 player_y = screen_height - player_height - 10
-
-# プレイヤーの速度
 player_speed = 7
 
 # プレイヤーの画像を設定
@@ -48,6 +47,17 @@ enemy_images = [
     pygame.image.load("img/zonbi2.png")
 ]
 
+# 特殊な敵の設定
+new_enemy_width = 400
+new_enemy_height = 400
+new_enemy_images = [
+    pygame.image.load("img/enemy_speed_20.png")
+]
+
+# 新しい敵の最後の出現時刻　＆　5秒ごとに新しい敵を生成
+new_enemy_spawn_time = pygame.time.get_ticks()
+new_enemy_spawn_interval = 5000
+
 # ゲームの初期化
 def initialize_game():
     global player_x, player_y, enemy_list
@@ -56,6 +66,7 @@ def initialize_game():
     enemy_list = []
 
 initialize_game()
+
 
 # ゲームオーバーのメッセージ表示
 font = pygame.font.Font(None, 36)
@@ -67,6 +78,8 @@ clock = pygame.time.Clock()
 game_over = False
 
 while not game_over:
+    current_time = pygame.time.get_ticks()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -80,7 +93,23 @@ while not game_over:
     keys = pygame.key.get_pressed()
     player_x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * player_speed
 
-    # 7秒ごとに速度が20の敵を生成
+    # 5秒ごとに新しい敵を生成
+    if current_time - new_enemy_spawn_time > new_enemy_spawn_interval:
+        new_enemy_x = random.randint(0, screen_width - new_enemy_width)
+        new_enemy_y = 0
+        new_enemy_speed = 20
+        new_enemy_image = random.choice(new_enemy_images)
+
+        scale_factor = max(0.5, min(3.0, 1 / new_enemy_speed))
+        scaled_width = int(new_enemy_width * scale_factor)
+        scaled_height = int(new_enemy_height * scale_factor)
+        scaled_image = pygame.transform.scale(new_enemy_image, (scaled_width, scaled_height))
+        enemy_list.append([new_enemy_x, new_enemy_y, new_enemy_speed, scaled_image])
+
+        # 最後に新しい敵が生成された時刻を更新
+        new_enemy_spawn_time = current_time
+
+    # 通常の敵を生成
     if len(enemy_list) < max_enemies:
         enemy_x = random.randint(0, screen_width - enemy_width)
         enemy_y = 0
@@ -95,28 +124,32 @@ while not game_over:
 
     # 敵の移動
     for enemy in enemy_list:
-        enemy[1] += enemy[2]  # 速度を考慮して移動
+        enemy[1] += enemy[2]  # 敵の縦（ｙ）座標に、敵[2]の速度を加える⇒敵が画面上部から下方向に移動
 
+    # 画面下端に到達した敵をリストから排除
     enemy_list = [enemy for enemy in enemy_list if enemy[1] < screen_height]
 
     for enemy in enemy_list:
         if(
-            player_x < enemy[0] + enemy_width
-            and player_x + player_width > enemy[0]
-            and player_y < enemy[1] + enemy_height
-            and player_y + player_height > enemy[1]
+            player_x < enemy[0] + enemy_width       # プレイヤの左端が敵の右端よりも左側にあるか
+            and player_x + player_width > enemy[0]  # プレイヤの右端が敵の左端よりも右側にあるか
+            and player_y < enemy[1] + enemy_height  # プレイヤの上端が敵の下端よりも上にあるか
+            and player_y + player_height > enemy[1] # プレイヤの下端が敵の上端よりも下にあるか
         ):
-            game_over = True
+            game_over = True  # 上記の全てが満たされた場合はプレイヤと敵が重なっているためゲームオーバー
 
-    
-    screen.blit(background_image, (0,0))
-    screen.blit(player_image, (player_x, player_y))
+    # 画像の描画
+    screen.blit(background_image, (0,0))  # background_image, (0,0)で画面全体を背景で覆う
+    screen.blit(player_image, (player_x, player_y))  
     for enemy in enemy_list:
-        screen.blit(enemy[3], (enemy[0], enemy[1]))
+        screen.blit(enemy[3], (enemy[0], enemy[1]))  # リスト内の各敵の画像を描画
 
+    # ゲームオーバー状態で、テキストを描画
     if game_over:
         screen.blit(game_over_text, ((screen_width - game_over_text.get_width()) // 2, (screen_height - game_over_text.get_height()) // 2))
 
+    # 画面の描画を反映。全ての描画が表示される
     pygame.display.flip()
      
+    # フレームレート制限（1秒当たり30フレーム）
     clock.tick(30)
---
+++ walking_RPG.py
@@ -0,0 +1,160 @@
+import pygame
+import sys
+import random
+import os
+from glob import glob
+
+# Pygameの初期化
+pygame.init()
+
+# 画面のサイズ
+WIDTH, HEIGHT = 800, 600
+
+# 色
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+# プレイヤークラス
+class Player:
+    def __init__(self, name, hp, mp, image_path, x, y):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.image = pygame.image.load(image_path)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def attack(self):
+        return random.randint(1, 10)
+
+    def cast_spell(self):
+        if self.mp >= 5:
+            self.mp -= 5
+            return random.randint(5, 15)
+        else:
+            print("銃弾が足りません!! 通常攻撃します。")
+            return self.attack()
+
+    def use_item(self):
+        return random.randint(1, 5)
+
+# プレイヤーと敵の数をランダムに決定
+num_players = 2
+num_enemies = random.randint(2, 5)
+
+# Pygameの画面初期化
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("WALKING_RPG Battle")
+
+# 背景画像の読み込み（PNGファイル）
+background = pygame.image.load("enemy_img/onigashima.png")
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+
+# フォントの設定
+font = pygame.font.Font(pygame.font.get_default_font(), 36)
+
+# プレイヤーの初期化
+players = []
+player_names = ["Rick", "Darryl"]
+for i in range(num_players):
+    player = Player(player_names[i], 30, 20, f"img/player{i+1}.png", 50, 200 + i * 100)
+    players.append(player)
+
+# 敵の初期化
+enemies = []
+
+images = glob(r"enemy_img/*")
+
+# 生成する敵の数を2体に固定
+num_enemies = 2
+
+# 敵の縮小率
+scale_factor = 0.8
+
+for i in range(num_enemies):
+    enemy_image_path = images[random.randint(0, len(images) - 1)]
+    
+    # 敵画像のサイズを取得
+    enemy_image = pygame.image.load(enemy_image_path)
+    enemy_width, enemy_height = enemy_image.get_size()
+    
+    # サイズを縮小
+    enemy_width = int(enemy_width * scale_factor)
+    enemy_height = int(enemy_height * scale_factor)
+    
+    # 画面右側の上部に2体の敵を表示し、重ならないように配置
+    initial_x = WIDTH - enemy_width
+    initial_y = i * (HEIGHT // (2 * num_enemies))  # 画面を垂直方向に均等に分割して配置
+    
+    # 画面からはみ出ないように再配置
+    while initial_y + enemy_height > HEIGHT or initial_y < 0:
+        initial_y = random.randint(0, HEIGHT - enemy_height)
+    
+    enemy = Player(f"monster{i+1}", 20, 10, enemy_image_path, initial_x, initial_y)
+    enemies.append(enemy)
+
+
+# 戦闘画面のメインループ
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # マウスクリックでコマンドを実行
+            x, y = event.pos
+            for player in players:
+                if player.rect.collidepoint(x, y):
+                    damage = player.attack()
+                    target_enemy = random.choice(enemies)
+                    target_enemy.hp -= damage
+                    print(f"{player.name}が{target_enemy.name}に{damage}のダメージを与えました！")
+
+    # プレイヤーのHPが0以下の場合
+    for player in players:
+        if player.hp <= 0:
+            game_over_text = font.render("Game Over!", True, RED)
+            screen.blit(game_over_text, (WIDTH // 2 - 100, HEIGHT // 2 - 50))
+            pygame.display.flip()
+            pygame.time.wait(5000)  # 5秒間メッセージ表示後に終了
+            pygame.quit()
+            sys.exit()
+
+    # 敵のHPが0以下の場合
+    for enemy in enemies:
+        if enemy.hp <= 0:
+            enemies.remove(enemy)
+
+    # 背景を描画
+    screen.blit(background, (0, 0))
+
+    # プレイヤーの画像表示
+    for player in players:
+        screen.blit(player.image, (player.rect.x, player.rect.y))
+    # 敵の画像表示と位置調整
+    for enemy in enemies:
+        # 敵が画面外にはみ出さないように位置を制限
+        enemy.rect.x = max(0, min(WIDTH - enemy.rect.width, enemy.rect.x))
+        enemy.rect.y = max(0, min(HEIGHT - enemy.rect.height, enemy.rect.y))
+        screen.blit(enemy.image, (enemy.rect.x, enemy.rect.y))
+
+
+    # コマンドボタンの表示
+    attack_button = pygame.draw.rect(screen, RED, (50, 450, 150, 50))
+    magic_button = pygame.draw.rect(screen, RED, (250, 450, 150, 50))
+    item_button = pygame.draw.rect(screen, RED, (450, 450, 150, 50))
+    escape_button = pygame.draw.rect(screen, RED, (650, 450, 150, 50))
+
+    # ボタンにラベルを表示
+    attack_label = font.render("Attack", True, WHITE)
+    magic_label = font.render("Handgun", True, WHITE)
+    item_label = font.render("Item", True, WHITE)
+    escape_label = font.render("Escape", True, WHITE)
+    screen.blit(attack_label, (80, 465))
+    screen.blit(magic_label, (280, 465))
+    screen.blit(item_label, (480, 465))
+    screen.blit(escape_label, (680, 465))
+
+    # 画面を更新
+    pygame.display.flip()
